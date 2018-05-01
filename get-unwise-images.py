07145a1680963414a88596884db9aeb1d98ea7fe
rfinn@siena.edu <rfinn@siena.edu>
2018-05-01 08:30:52 -0400

    cleaning up code

---
+++ get-unwise-images.py
@@ -0,0 +1,124 @@
+#! /usr/bin/env python
+
+"""
+GOAL:
+- to retrieve unWISE images of galaxies given their NSA id
+
+PROCEDURE:
+- query Halpha directory
+- get list of NSAIDs
+- read in NSA
+
+USEAGE:
+
+from within ipython on laptop
+
+ %run ~/github/virgowise/get-unwise-image-Halpha.py --nsapath '/Users/rfinn/research/NSA/'
+
+REQUIRED FILES:
+- nsa fits catalog
+
+
+NOTES:
+I want to download the unWISE images with the same NGC number as the Halpha images. I can do this by splitting up the radial output filenames and indexing just the nsatlas number of the galaxy,gonna need a way to convert that to the number in NED but for now: 
+
+-1/28/18 I want to somehow download the tar files from unWISE but know what galaxy is which by outputting the nsatlas numbers which is what our Halpha images are in. Also commenting details of code.
+
+
+"""
+
+import sys
+import os
+from astropy.io import fits
+import numpy as np
+from astroquery.sdss import SDSS
+from astroquery.ned import Ned
+from astropy import coordinates as coords
+import astropy.units as u
+from matplotlib import pyplot as plt
+from astropy.io import fits
+import wget
+from scipy.stats import scoreatpercentile
+import tarfile
+import argparse
+import glob
+import gzip
+
+
+parser = argparse.ArgumentParser(description ='Download unWISE images for specific NSA galaxies')
+parser.add_argument('--band', dest = 'band', default = '3', help = 'unWISE image band to download: 3=12um, 4=22um (can only do one at a time for now.')
+parser.add_argument('--nsapath', dest = 'nsapath', default = '/Users/rfinn/github/Virgo/tables/', help = 'full path to Virgo NSA catalog (nsa.virgo.fits)')
+args = parser.parse_args() #brings in these arguments above
+
+
+###
+### READ IN NSA CATALOG
+###
+
+nsatab = args.nsapath+'nsa_v0_1_2.fits'
+nsa = fits.getdata(nsatab)
+nsadict = dict((a,b) for a,b in zip(nsa.NSAID,np.arange(len(nsa.NSAID))))
+
+###
+### GET NSA IDS FOR IMAGES TO DOWNLOAD
+###
+
+# hardcoding the list of NSA ids
+# will make this smarter over the summer
+# could run this on ALL galaxies in NSA virgo catalog!
+
+#nsaid = ['56462','67595','164358','54578','61690','61692']
+#nsaid = ['143986']
+
+ngc_filament_ids = [56403,56409,56410,56411,56434,56455,56456,56462,56469,56482,56489,61690,61691,61692,67593,88353,90371,93403,94217,104307,104439,118647,119230,119289,120018,120053,142509,143682,143686,143827,143951,143986,162674,163136,163783,164224,164358]
+
+nsaid = ngc_filament_ids
+
+
+###
+### START OF CODE TO DOWNLOAD IMAGES
+###
+
+
+baseurl = 'http://unwise.me/cutout_fits?version=allwise'  
+imsize = '100' # max size = 256 pixels
+bands='4'
+
+
+for id in nsaid:
+#for j in range(1):
+    #id = nsaid[j]
+    i = nsadict[int(id)]
+    print i
+    print 'RA= ',nsa.RA[i]
+    print 'DEC= ',nsa.DEC[i]
+    ra = nsa.RA[i]
+    dec = nsa.DEC[i]
+    imurl = baseurl+'&ra=%.5f&dec=%.5f&size=%s&bands=%s'%(ra,dec,imsize,bands)
+    print imurl
+    wisetar = wget.download(imurl)  #trying to add in nsaid
+    
+    
+    tartemp = tarfile.open(wisetar,mode='r:gz') #mode='r:gz'
+    #tartemp = tartemp.add(id)
+    wnames = tartemp.getnames()
+    #wnames.append(nsaid)
+    wmembers = tartemp.getmembers()
+    #path = '/home/share/research/Virgo/Galfit2018'
+    tartemp.extractall()
+    for a in wmembers:
+        os.rename(a.name, str(id)+'-'+a.name)    
+
+    #rename = []
+
+    
+    
+    for i in wnames:
+        split = i.split('-')
+        print split
+    
+
+
+
+
+
